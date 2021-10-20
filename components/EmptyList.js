b0e57d69555fa42f138e6f21e7dd8a8fd7ef8487
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-10-20 13:48:06 +0000

    Update EmptyList.js

--- components/EmptyList.js
+++ components/EmptyList.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-import image from '../assets/todo.png';
+import image from '../assets/girl.png';
 
+const warning  = "Nothing to see here..."
 const EmptyList = () => (
   <View style={styles.container}>
     <ImageBackground
       source={image}
       resizeMode="cover"
       style={styles.image}></ImageBackground>
-    <Text style={styles.text}>No tasks</Text>
+    <Text style={styles.text}>{warning}</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center"
+    flex: 1,    
+    marginTop: 80
   },
   image: {
     flex: 1,
     justifyContent: 'center',
-    height: 200,
-    flexDirection: 'column',
-    marginTop:100
+    height: 300
+    
   },
   text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
+    color: 'grey',
+    fontSize: 18,
+    lineHeight: 20,
     height: '100%',
-    fontWeight: 'bold',
     textAlign: 'center',
   },
 });
