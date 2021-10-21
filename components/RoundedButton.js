0caccb5ba770c4a637a81968370bd0b4000b94dc
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-10-21 16:27:37 +0000

    Update RoundedButton.js

--- components/RoundedButton.js
+++ components/RoundedButton.js
@@ -1,5 +1,6 @@
-import * as React from 'react'
-import { TouchableOpacity, Text, StyleSheet, View } from 'react-native'
+import * as React from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 export const RoundedButton = ({
   style = {},
@@ -11,13 +12,12 @@ export const RoundedButton = ({
     <View>
       <TouchableOpacity
         style={[styles(size).radius, style]}
-        onPress={props.onPress}
-      >
-        <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
+        onPress={props.onPress}>
+        <Icon name={props.iconName} type={props.iconType} color={props.iconColor} />
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = (size) =>
   StyleSheet.create({
@@ -34,4 +34,4 @@ const styles = (size) =>
       color: '#000',
       fontSize: size / 3,
     },
-  })
+  });
