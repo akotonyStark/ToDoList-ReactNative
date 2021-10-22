50b49db3cef6518cc822d8678b8965464759d9aa
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-10-22 18:00:00 +0000

    Update Task.js

--- components/Task.js
+++ components/Task.js
@@ -1,20 +1,37 @@
 import * as React from 'react';
 import { Text, View, CheckBox, StyleSheet, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { RadioButton } from 'react-native-paper';
 
 export default function Task(props) {
-  const [isSelected, setSelection] = React.useState(false);
-
+  const [checked, setChecked] = React.useState(false);
+ 
   return (
     <View style={[styles.item, styles.card, styles.shadowProp]}>
       <View style={styles.task}>
-        <CheckBox
-          value={isSelected}
-          onValueChange={setSelection}
-          style={styles.checkbox}
+        <Icon
+          name="description"
+          type="material"
+          color="#2980B9"          
         />
-        <Text style={{display:"flex", flexDirection: "row", justifyContent: "flex-start"}}>{props.name}</Text>
-        <View style={{position: 'absolute',   right: 0, bottom: 0, justifyContent: 'right', alignSelf: 'right'}}>
+        <Text
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            marginLeft: 15,
+            fontSize: 18
+          }}>
+          {props.name}
+        </Text>
+        <View
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            justifyContent: 'right',
+            alignSelf: 'right',
+          }}>
           <Icon
             name="more-vert"
             type="material"
@@ -32,9 +49,8 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'row',
-    //justifyContent: 'flex-end'   
-    
-  }, 
+    //justifyContent: 'flex-end'
+  },
   checkbox: {
     alignSelf: 'right',
     marginRight: 10,
